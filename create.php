d93844f4ab06c98ad24b3dd0f215da6c5c964a48
Phillip Ferland <pferland@randomintervals.com>
2017-01-18 22:25:38 -0500

    start work of create recipe page

---
+++ create.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Control Panel
+create.php
+Copyright (C) 2013 Phil Ferland
+
+This program is free software; you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+ou should have received a copy of the GNU General Public License along with this program;
+if not, write to the
+
+   Free Software Foundation, Inc.,
+   59 Temple Place, Suite 330,
+   Boston, MA 02111-1307 USA
+*/
+
+require "lib/config.php"; #www config
+require "lib/CapoDBFront.inc.php"; #CapoDB Front end class
+
+$CapoDB = new CapoDBFront($WWWconfig);
+$CapoDB->smarty->display("create_new.tpl");
